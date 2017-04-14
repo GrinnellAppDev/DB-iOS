407ee415392f1648df1eb07e265230fe5d0f3681
Devin Dooley <devinadooley@gmail.com>
2017-04-13 20:24:00 -0500

    black placeholder text

--- DB/GADNumberTableViewCell.m
+++ DB/GADNumberTableViewCell.m
@@ -23,7 +23,7 @@
     self.selectedBackgroundView = backView;
     
     [super setSelected:selected animated:animated];
-    self.numberField.attributedPlaceholder =[[NSAttributedString alloc] initWithString: self.placeholderText attributes:@{NSForegroundColorAttributeName: [UIColor colorWithRed:0.73 green:0.29 blue:0.29 alpha:1.0]}];
+    self.numberField.attributedPlaceholder =[[NSAttributedString alloc] initWithString: self.placeholderText attributes:@{NSForegroundColorAttributeName: [UIColor blackColor]}];
     // Configure the view for the selected state
     
     [self.numberField setKeyboardType:UIKeyboardTypeNumberPad];
