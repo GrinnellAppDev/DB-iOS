8ae4857c2bbaeec93471272ae3aa9f204cd91fcc
Devin Dooley <devinadooley@gmail.com>
2018-02-11 12:24:09 -0600

    Dummy data updated to reflect API and fix detail view

--- DB/GADPerson.m
+++ DB/GADPerson.m
@@ -71,12 +71,13 @@
     person2.lastName=@"Osera";
     person2.email=@"oserapet@grinnell.edu";
     person2.address=@"3811 Science";
+    person2.box=@"SCIE";
     person2.phone=@"4010";
     person2.city=@"Grinnell";
     person2.state=@"IA";
     person2.zip=@"50112";
     person2.offCampusAddress=@[@"1804 3rd Ave"];
-    person2.title=@[@"Assistant Professor of Computer Science"];
+    person2.title=@[@"Assistant Professor of Computer Science", @"", @"", @"", @"", @"Computer Science"];
     person2.imgPath=[NSURL URLWithString:@"https://itwebapps.grinnell.edu/PcardImages/moved/84326.jpg"];
     
     person3.firstName=@"Anita";
@@ -85,6 +86,7 @@
     person3.major=@"Computer Science/Music";
     person3.email=@"dewittan17@grinnell.edu";
     person3.offCampusAddress=@[@"347 Drake Ave"];
+    person3.address=@"Hannibal Kershaw Hall 2349";
     person3.city=@"Bolingbrook";
     person3.state=@"IL";
     person3.zip=@"60490-3103";
