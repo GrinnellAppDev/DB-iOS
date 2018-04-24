7b6709bac7b87865360064d8a4ea52e23cf9cdf6
arthurhero <chenziwe@grinnell.edu>
2018-04-23 22:03:59 -0500

    modified GADQuery according to new API

--- GADDirectory/GADQuery.h
+++ GADDirectory/GADQuery.h
@@ -5,16 +5,16 @@
 
 @property (nullable,nonatomic, strong) NSString *lastName;
 @property (nullable,nonatomic, strong) NSString *firstName;
-@property (nullable,nonatomic, strong) NSString *campusAddressOrPOBox;
-@property (nullable,nonatomic, strong) NSString *FacStaffDeptOffice;
-@property (nullable,nonatomic, strong) NSString *studentMajor;
-@property (nullable,nonatomic, strong) NSString *hiatus;
-@property (nullable,nonatomic, strong) NSString *computerUsername;
+@property (nullable,nonatomic, strong) NSString *userName;
 @property (nullable,nonatomic, strong) NSString *campusPhone;
+@property (nullable,nonatomic, strong) NSString *campusAddress;
 @property (nullable,nonatomic, strong) NSString *homeAddress;
-@property (nullable,nonatomic, strong) NSString *SGA;
+@property (nullable,nonatomic, strong) NSString *classYr;
+@property (nullable,nonatomic, strong) NSString *facStaffOffice;
+@property (nullable,nonatomic, strong) NSString *major;
 @property (nullable,nonatomic, strong) NSString *concentration;
-@property (nullable,nonatomic, strong) NSString *studentClass;
+@property (nullable,nonatomic, strong) NSString *sga;
+@property (nullable,nonatomic, strong) NSString *hiatus;
 
 /*!
  @brief This method takes in a GADQuery containing criteria and returns an array of GADPerson which fulfill the criteria. Username and Password required.
@@ -31,11 +31,5 @@
 -(void) executeWithUsername:(nonnull NSString*)username
                    Password:(nonnull NSString*) password
           completionHandler:(void(^_Nonnull)(NSArray<GADPerson *> *people,NSError *error))completion;
-
-
-+ (void) executeWithDict:(nonnull NSDictionary*)criteria
-                Username:(nonnull NSString*)username
-                Password:(nonnull NSString*)password
-       completionHandler:(void(^_Nonnull)(NSArray<GADPerson *> *people,NSError *error))completion;
 
 @end
--- GADDirectory/GADQuery.m
+++ GADDirectory/GADQuery.m
@@ -1,6 +1,6 @@
 #import "GADQuery.h"
 
-static const NSString *apiURL = @"https://itwebappstest.grinnell.edu/DotNet/WebServices/api/db";
+static const NSString *apiURL = @"https://appdev.grinnell.edu/api/db/v1/list";
 const NSTimeInterval timeoutInterval = 60.0;
 
 @implementation GADQuery
@@ -9,16 +9,21 @@ const NSTimeInterval timeoutInterval = 60.0;
     NSMutableDictionary *dict = [NSMutableDictionary new];
     if (self.lastName) [dict setValue:self.lastName forKey:@"lastName"];
     if (self.firstName) [dict setValue:self.firstName forKey:@"firstName"];
-    if (self.campusAddressOrPOBox) [dict setValue:self.campusAddressOrPOBox forKey:@"box"];
-    if (self.FacStaffDeptOffice) [dict setValue:self.FacStaffDeptOffice forKey:@"titkle5"];
-    if (self.studentMajor) [dict setValue:self.studentMajor forKey:@"major"];
+    if (self.userName) [dict setValue:self.userName forKey:@"userName"];
+
+    if (self.campusPhone) [dict setValue:self.campusPhone forKey:@"campusPhone"];
+    if (self.campusAddress) [dict setValue:self.campusAddress forKey:@"campusAddress"];
+    if (self.homeAddress) [dict setValue:self.homeAddress forKey:@"homeAddress"];
+
+    if (self.classYr)[dict setValue:self.classYr forKey:@"classYr"];
+
+    if (self.facStaffOffice) [dict setValue:self.facStaffOffice forKey:@"facStaffOffice"];
+    
+    if (self.major) [dict setValue:self.major forKey:@"major"];
+    if (self.concentration) [dict setValue:self.concentration forKey:@"concentration"];
+
+    if (self.sga) [dict setValue:self.sga forKey:@"sga"];
     if (self.hiatus) [dict setValue:self.hiatus forKey:@"hiatus"];
-    if (self.computerUsername) [dict setValue:self.computerUsername forKey:@"userName"];
-    if (self.campusPhone) [dict setValue:self.campusPhone forKey:@"office_phone"];
-    if (self.homeAddress) [dict setValue:self.homeAddress forKey:@"adrees"];
-    if (self.SGA) [dict setValue:self.SGA forKey:@"title"];
-    if (self.concentration) [dict setValue:self.concentration forKey:@"minor"];
-    if (self.studentClass)[dict setValue:self.studentClass forKey:@"classYear"];
     return dict;
 }
 
@@ -42,19 +47,36 @@ const NSTimeInterval timeoutInterval = 60.0;
     components.queryItems = queryItems;
     NSURL *url = components.URL;
     
+    /* old authentication scheme
     NSString *authenticationString = [NSString stringWithFormat:@"{'un':'%@', 'pw':'%@'}",username,password];
     NSData *bodyData = [authenticationString dataUsingEncoding:NSUTF8StringEncoding];
     NSString *bodyLength = [NSString stringWithFormat:@"%lu",(unsigned long)[bodyData length]];
+    */
     
     NSMutableURLRequest *req = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:timeoutInterval];
-    [req setHTTPMethod:@"POST"];
-    [req setValue:bodyLength forHTTPHeaderField:@"Content-Length"];
+    [req setHTTPMethod:@"GET"];
+    //[req setValue:bodyLength forHTTPHeaderField:@"Content-Length"];
     [req setValue:@"application/json" forHTTPHeaderField: @"Content-Type"];
-    [req setHTTPBody:bodyData];
+    //[req setHTTPBody:bodyData];
     
     NSURLSessionDataTask *task = [[NSURLSession sharedSession]dataTaskWithRequest:req completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (error) {
             completion(nil,error);
+            return;
+        }
+        NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *) response;
+        if ([httpResponse statusCode] != 200) {
+            NSDictionary *errorDict = [NSJSONSerialization JSONObjectWithData:data options:0 error:&JSONParsingError];
+            NSDictionary *userInfo =
+  @{
+    NSLocalizedDescriptionKey: NSLocalizedString(@"Search unsuccessful.", nil),
+    NSLocalizedFailureReasonErrorKey: NSLocalizedString([errorDict valueForKey:@"message"], nil),
+    NSLocalizedRecoverySuggestionErrorKey: NSLocalizedString(@"", nil)
+    };
+             NSError *searchError = [NSError errorWithDomain:@""
+                                                 code:0
+                                             userInfo:userInfo];
+            completion(nil,searchError);
             return;
         }
         NSMutableArray <GADPerson*> *result=[NSMutableArray<GADPerson*> new];
